e1b4eaf77ea0f04903f91d506af1c560c9a0a921
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-07-26 01:29:28 -0300

    Adicionando Teste de Conexão e Teste de Select

---
+++ Testes/testeConexao.php
@@ -0,0 +1,23 @@
+<?php
+// Teste apenas para testar conexão com banco de dados 
+
+  $host = '127.0.0.1';
+  $db = 'phppdo';
+  $user = 'user';
+  $pass = '123';
+  $charset = 'utf8mb4';
+  
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $options = [
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES   => false,
+  ];
+  
+  try {
+      $pdo = new PDO($dsn, $user, $pass, $options);
+      echo "Conexão bem-sucedida!";
+  } catch (\PDOException $e) {
+      echo 'Erro: ' . $e -> getMessage();
+  }
+?>
